0f8f75dec446725b12952eaea79112c2b4b64f0a
urasaki keisuke <213427006@ccmailg.meijo-u.ac.jp>
2021-07-25 14:54:59 +0900

    add jit

--- nn/relu.py
+++ nn/relu.py
@@ -2,6 +2,9 @@ from .module import Module
 
 import numpy
 
+from numba import jit
+
+
 class ReLU(Module):
     def __init__(
         self,
@@ -12,6 +15,7 @@ class ReLU(Module):
         self.inplace: bool = inplace
         self.mask: numpy.ndarray = None
 
+    @jit
     def forward(self, input: numpy.ndarray) -> numpy.ndarray:
         self.mask = (input <= 0)
 
@@ -23,6 +27,7 @@ class ReLU(Module):
 
         return output
 
+    @jit
     def backward(self, dout: numpy.ndarray) -> numpy.ndarray:
         dout[self.mask] = 0
         dx: numpy.ndarray = dout
